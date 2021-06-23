c9fa7feb32434c6a846feda66f1c1642973352c0
Lokesh Jindal <ljindal@linkedin.com>
2021-06-23 16:38:18 -0700

    Finish the implementation. Important lesson: Use main method to avoid polluting the global namespace with variables

--- course4_using_databse_with_python/twfriends/twfriends.py
+++ course4_using_databse_with_python/twfriends/twfriends.py
@@ -24,39 +24,94 @@ CTX = ssl.create_default_context()
 CTX.check_hostname = False
 CTX.verify_mode = ssl.CERT_NONE
 
+def addNewUser(uname):
+    DB_CURSOR.execute("INSERT OR IGNORE INTO People (name, retrieved) Values (?, ?)", (uname, 0))
+    DB_CONN.commit()  # important so that subsequent SELECTs can find the entry
+    
+def setUserAsRetrieved(uname):
+    DB_CURSOR.execute("UPDATE People SET retrieved = 1 WHERE name = (?)", (uname, ))
+    DB_CONN.commit()
+
+def getUserInfo(user):
+    DB_CURSOR.execute("SELECT * FROM People WHERE name = (?)", (user,))
+    return DB_CURSOR.fetchone()
+
+def getUserId(user):
+    (uid, uname, retrieved) = getUserInfo(user)
+    return uid
+
+def addNewConnection(uname, fname):
+    uid = getUserId(uname)
+    fid = getUserId(fname)
+    print(f"Adding connection from {uid}:{uname} to {fid}:{fname}")
+    DB_CURSOR.execute("INSERT OR IGNORE INTO Follows (from_id, to_id) Values (?, ?)", (uid, fid))
+    DB_CONN.commit()
+
 def getNextUser(uname):
-    uid = -1
     if uname:
-        # insert the user if needed, retrieve if not already retrieved
-        DB_CURSOR.execute("INSERT OR IGNORE INTO People (name, retrieved) Values (?, ?)", (uname, 0))
-        DB_CONN.commit()  # important so that subsequent SELECTs can find the entry
-        DB_CURSOR.execute("SELECT * FROM People WHERE name = (?)", (uname,))
-        (uid, uname, retrieved) = DB_CURSOR.fetchone()
+        try:
+            (uid, uname, retrieved) = getUserInfo(uname)
+        except Exception as e:
+            # user does not exist already
+            return uname
         if (retrieved):
-            (uid, uname) = (-1, "")
-        else:
-            DB_CURSOR.execute("UPDATE People SET retrieved = 1 WHERE id = (?)", (uid, ))
+            print(f"uname: {uname} has already been retrieved")
+            uname = ""
     else:
         # find a user that has not been retrieved yet
-        DB_CURSOR.execute("SELECT id, name FROM People WHERE retrieved = 0 LIMIT 1")
+        DB_CURSOR.execute("SELECT name FROM People WHERE retrieved = 0 LIMIT 1")
         try:
-            (uid, uname) = DB_CURSOR.fetchone()
+            uname = DB_CURSOR.fetchone()[0]
         except:
-            (uid, uname) = (-1,"")
-    return (uid, uname)
+            uname = ""
+    return uname
 
-while True:
-    uname = input("Enter user name or type 'quit':")
-    uname = uname.strip()
-    if (uname == "quit"):
-        break
-    (uid, uname) = getNextUser(uname)
+def getTwitterFriends(uname):
+    TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+    url = augment(TWITTER_URL, {'screen_name': uname, 'count': '5'})
+    try:
+        connection = urllib.request.urlopen(url, context=CTX)
+    except Exception as e:
+        print(f"Could not retrieve user: {uname} error: [{e}]")
+        return []
     
-    if (not uname):
-        print(f"No user to retrieve.")
-        continue
-    print(f"Will retrieve uid: {uid} uname: {uname}")
+    # fetch was successful
+    addNewUser(uname)
+    setUserAsRetrieved(uname)
 
+    data = connection.read().decode()
+    headers = dict(connection.getheaders())
+
+    print(f"Remaining: [{headers['x-rate-limit-remaining']}]")
+    try:
+        js = json.loads(data)
+    except:
+        print(f'ERROR: Unable to parse json: [{data}]')
+        return []
+    if 'users' not in js:
+        print(f"ERROR: could not find 'users' in: [{js}]")
+        return []
+    return js['users']
+
+def main():
+    while True:
+        uname = input("Enter user name or type 'quit':")
+        uname = uname.strip()
+        if (uname == "quit"):
+            break
+        uname = getNextUser(uname)
+        
+        if (not uname):
+            print(f"No user to retrieve.")
+            continue
+        print(f"Will retrieve uname: {uname}")
     
+        friends = getTwitterFriends(uname)
+        for f in friends:
+            fname = f['screen_name']
+            print(fname)
+            addNewUser(fname)
+            addNewConnection(uname, fname)
 
-
+if __name__ == "__main__":
+    main()
